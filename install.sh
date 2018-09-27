832c962d15d7fcb4b912ca4d21da2ea287790ce0
Anemone95 <x565178035@126.com>
2018-09-27 18:48:22 +0800

    add install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+SCRIPT_PATH="$( cd "$( dirname "$0"  )" && pwd  )"
+ln -f $SCRIPT_PATH/.bashrc $HOME/.bashrc
+ln -f $SCRIPT_PATH/.bash-prompt.sh $HOME/.bash-prompt.sh
+ln -f $SCRIPT_PATH/.gitconfig $HOME/.gitconfig
+ln -f $SCRIPT_PATH/.git-prompt.sh $HOME/.git-prompt.sh
+ln -f $SCRIPT_PATH/.git-completion.sh $HOME/.git-completion.sh
+ln -f $SCRIPT_PATH/.profile $HOME/.profile
