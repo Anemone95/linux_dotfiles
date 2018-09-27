be7177541d57078c7f1f76ea8dbf56175e62453d
Anemone95 <x565178035@126.com>
2018-09-27 19:18:03 +0800

    add bitbucket ssh

--- install.sh
+++ install.sh
@@ -5,3 +5,5 @@ ln -f $SCRIPT_PATH/.gitconfig $HOME/.gitconfig
 ln -f $SCRIPT_PATH/.git-prompt.sh $HOME/.git-prompt.sh
 ln -f $SCRIPT_PATH/.git-completion.sh $HOME/.git-completion.sh
 ln -f $SCRIPT_PATH/.profile $HOME/.profile
+mv $HOME/.ssh $HOME/.ssh.bk
+ln -f -s $SCRIPT_PATH/.ssh $HOME/.ssh
