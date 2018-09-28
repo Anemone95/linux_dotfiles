6d09df86f77564ebb9921e1f5bcea05928881fd6
Anemone95 <x565178035@126.com>
2018-09-28 10:56:34 +0800

    add bitbucket password

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ ln -f $SCRIPT_PATH/.gitconfig $HOME/.gitconfig
 ln -f $SCRIPT_PATH/.git-prompt.sh $HOME/.git-prompt.sh
 ln -f $SCRIPT_PATH/.git-completion.sh $HOME/.git-completion.sh
 ln -f $SCRIPT_PATH/.profile $HOME/.profile
+ln -f $SCRIPT_PATH/.netrc $HOME/.netrc
 if [ -e $HOME/.ssh ];then
     echo "Backup ~/.ssh to ~/.ssh.bk"
     mv $HOME/.ssh $HOME/.ssh.bk
