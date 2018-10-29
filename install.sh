8f7c3ed0cc542d2eefb63475d6076270970633de
Anemone95 <x565178035@126.com>
2018-10-29 14:48:05 +0800

    add makedir local/bin

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ ln -f $SCRIPT_PATH/.git-prompt.sh $HOME/.git-prompt.sh
 ln -f $SCRIPT_PATH/.git-completion.sh $HOME/.git-completion.sh
 ln -f $SCRIPT_PATH/.profile $HOME/.profile
 ln -f $SCRIPT_PATH/.netrc $HOME/.netrc
+mkdir -p $HOME/.local/bin
 if [ -e $HOME/.ssh ];then
     echo "Backup ~/.ssh to ~/.ssh.bk"
     mv $HOME/.ssh $HOME/.ssh.bk
