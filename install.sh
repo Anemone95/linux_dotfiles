9dcfe84f0ffebf7d9313d9bf9f136197982558a9
Anemone95 <x565178035@126.com>
2018-11-07 21:09:49 +0800

    symbol link

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 SCRIPT_PATH="$( cd "$( dirname "$0"  )" && pwd  )"
-ln -f $SCRIPT_PATH/.bashrc $HOME/.bashrc
-ln -f $SCRIPT_PATH/.bash-prompt.sh $HOME/.bash-prompt.sh
-ln -f $SCRIPT_PATH/.gitconfig $HOME/.gitconfig
-ln -f $SCRIPT_PATH/.git-prompt.sh $HOME/.git-prompt.sh
-ln -f $SCRIPT_PATH/.git-completion.sh $HOME/.git-completion.sh
-ln -f $SCRIPT_PATH/.profile $HOME/.profile
-ln -f $SCRIPT_PATH/.netrc $HOME/.netrc
+ln -f -s $SCRIPT_PATH/.bashrc $HOME/.bashrc
+ln -f -s $SCRIPT_PATH/.bash-prompt.sh $HOME/.bash-prompt.sh
+ln -f -s $SCRIPT_PATH/.gitconfig $HOME/.gitconfig
+ln -f -s $SCRIPT_PATH/.git-prompt.sh $HOME/.git-prompt.sh
+ln -f -s $SCRIPT_PATH/.git-completion.sh $HOME/.git-completion.sh
+ln -f -s $SCRIPT_PATH/.profile $HOME/.profile
+ln -f -s $SCRIPT_PATH/.netrc $HOME/.netrc
 mkdir -p $HOME/.local/bin
 if [ -e $HOME/.ssh ];then
     echo "Backup ~/.ssh to ~/.ssh.bk"
