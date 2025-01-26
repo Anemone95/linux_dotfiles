95d5fd3dd178f699dae37c84959d5c7ead5a0191
WenyuanXu <wenyuan.xu@cs.au.dk>
2025-01-26 16:24:24 +0100

    fix scpd

--- shconfig.sh
+++ shconfig.sh
@@ -86,6 +86,7 @@ if [[ $OS = "OSX" ]]; then
         # export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.aliyun.com/homebrew/homebrew-bottles
         eval "$(/opt/homebrew/bin/brew shellenv)"
     fi
+    export HOMEBREW_NO_AUTO_UPDATE=1
 fi
 
 source $HOME/.localconfig.sh
